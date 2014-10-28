b8394e69bf3e76797b48488eae6709218197b7e5
daniboomerang <estevez.dani@gmail.com>
2014-10-28 22:43:20 +0100

    First Commit for the responsive Layout: It is a side menu on the left side, that collapses automatically for small screen devices, a toogle booton (no functionallty still) that will toogle a responsive side menu on the right side. Tested on firefox, safari and iphone. Centered content is responsive, and navbar as well

--- client/scripts/controllers.js
+++ client/scripts/controllers.js
@@ -1,5 +1,23 @@
 var daniboomerangControllers = angular.module('daniboomerangControllers', []);
 
+daniboomerangControllers.controller('LayoutController', function($scope){
+
+	init();
+
+	function init(){
+		$scope.isToggledMenu = false;
+	}
+
+});
+
 daniboomerangControllers.controller('NavController', function($scope){
+	
+	$scope.toggleMenu = function(){
+		$scope.$parent.isToggledMenu = !$scope.$parent.isToggledMenu;
+	}
+
+	$scope.isToggledMenu = function(){
+		return $scope.$parent.isToggledMenu;
+	}
 
 });
