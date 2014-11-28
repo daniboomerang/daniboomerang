b04500fbf4bb89c231482017c23dda29ad0516bf
daniboomerang <estevez.dani@gmail.com>
2014-11-28 20:53:28 +0100

    Grunt && CircleCi && Heroku: Angular is not found...Adding time out on prepare on load to e2e.conf. May be like this angular will have time to be loaded...

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -8,11 +8,16 @@ exports.config = {
     maxInstances: 2
   },
 
+	onPrepare: function() {	
+		// implicit and page load timeouts
+		browser.manage().timeouts().pageLoadTimeout(40000);
+		browser.manage().timeouts().implicitlyWait(25000);
+ 	},
 
-  chromeDriver: '../node_modules/protractor/selenium/chromedriver',
+  	chromeDriver: '../node_modules/protractor/selenium/chromedriver',
 
-  baseUrl: 'http://localhost:8080/',
+  	baseUrl: 'http://localhost:8080/',
 
-  framework: 'jasmine',
+  	framework: 'jasmine',
 
 };
