47514851a80f9b7fdb0c6d399c07955c1490fd9b
daniboomerang <estevez.dani@gmail.com>
2014-12-20 12:24:55 +0100

    Designing/planning the remaining unit and e2e tests

---
+++ test/e2e/scrollingSpec.js
@@ -0,0 +1,102 @@
+'use strict';
+
+describe('Daniboomerang - scrolling scenario', function() {
+
+////////////////
+/// ON COVER ///
+////////////////
+
+/////////////////////////////////////////
+// If click to navbar section Who I am //
+/////////////////////////////////////////
+
+// it scrolls to Who I am section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+///// Back to cover /////
+
+/////////////////////////////////////////////
+// If clicks to navbar section What I like //
+/////////////////////////////////////////////
+
+// it scrolls to What I like section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+///// Back to cover /////
+
+////////////////////////////////////////////////
+// If clicks to navbar section What I've done //
+////////////////////////////////////////////////
+
+// it scrolls to What I've done section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+///// Back to cover /////
+
+//////////////////////////////////////////////////
+// If clicks to navbar section What I've learnt //
+//////////////////////////////////////////////////
+
+// it scrolls to What I've learnt section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+////////////////
+// ON CONTENT //
+////////////////
+
+///// go to Who I am  /////
+
+/////////////////////////////////////////////
+// If clicks to navbar section What I like //
+/////////////////////////////////////////////
+
+// it scrolls to What I like section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+//////////////////////////////////////////
+// If clicks to navbar section Who I am //
+//////////////////////////////////////////
+
+// it scrolls to Who I am section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+////////////////////////////////////////////////
+// If clicks to navbar section What I've done //
+////////////////////////////////////////////////
+
+// it scrolls to What I've done section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+//////////////////////////////////////////////////
+// If clicks to navbar section What I've learnt //
+//////////////////////////////////////////////////
+
+// it scrolls to What I've learnt section (check url)
+
+// header is expanded 
+
+// navbar section gets 'active'
+
+});
+
