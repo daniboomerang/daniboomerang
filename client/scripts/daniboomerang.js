58d7264d7b65df7fd133b6d17ae78c6ae455aaa9
daniboomerang <estevez.dani@gmail.com>
2015-07-31 11:48:34 +0200

    Intro: Adding 'Press ESC to skip' functionality

--- client/scripts/daniboomerang.js
+++ client/scripts/daniboomerang.js
@@ -30,17 +30,27 @@ angular.module('daniboomerangApp', [
 		link: function (scope, element, attrs) { scope.$on('app-starts', function($event){ element.removeClass('visibility-hidden'); }); }
 	}  
 })
-.directive('daniboomerangIntro', function($timeout, $rootScope, $compile) {
+.directive('daniboomerangIntro', function($timeout, $rootScope, $compile, $document) {
 	return {
 		restrict: 'A',
 		templateUrl: 'views/intro.html',
 		link: function (scope, element, attrs) {
+			function startApp(){
+				intro.addClass('fadeOut');
+				$timeout(function() { element.remove();
+				$rootScope.$broadcast('app-starts'); }, 1000);	
+			}
 			var intro = element.find('#intro');
 			var introElements = element.find('.intro-element');
 			var introCenter = element.find('#intro-center');
 			var introBoomerangWrapper = element.find('#intro-boomerang-wrapper');
 			var introBoomerang = element.find('#intro-boomerang');
-			scope.$on('active-section:cover', function($event){ 
+			var esc = element.find('#esc');
+
+			$document.bind("keyup", function(event) {
+        		if (event.keyCode === 27) { startApp(); }
+    		});
+			scope.$on('active-section:cover', function($event){
 				intro.addClass('visibility-visible');
         		intro.addClass('fadeIn');
         		introElements.addClass('visibility-visible');
@@ -50,8 +60,10 @@ angular.module('daniboomerangApp', [
 			});
 			$timeout(function() {
 				introBoomerangWrapper.addClass('rotateOut');
+				esc.addClass('rubberBand');
 				$timeout(function() { 
 					introBoomerangWrapper.remove();
+					esc.remove();
 					var introButtonStartAppHtml = '<div button id="start-button" size="md" content-type="text" text="START" ng-click-function="startApp()"></div>';
 					introCenter.append(introButtonStartAppHtml);
 					var introButtonStartApp = element.find('#start-button');
@@ -59,13 +71,7 @@ angular.module('daniboomerangApp', [
 					$compile(introCenter)(scope);
 				}, 500);
 			}, 10000);
-			scope.startApp = function(){
-				intro.addClass('fadeOut');
-				$timeout(function() { 
-					element.remove();
-					$rootScope.$broadcast('app-starts');
-				}, 1000);	
-			}
+			scope.startApp = function(){ startApp(); }
 		}
-	}  
+	}
 })
