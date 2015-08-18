7348462680db9387bfced7a78baa67ba5dd9a6a9
daniboomerang <estevez.dani@gmail.com>
2015-08-18 21:29:02 +0200

    Uncommenting tests. Running all tests

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -1,27 +1,27 @@
 exports.config = {
   /*specs: [], capabilities: { browserName: 'chrome', shardTestFiles: true, maxInstances: 2 },*/
   multiCapabilities: [  
-    { 
+    /*{ 
        'browserName': 'chrome',
        'chromeOptions' : {
         args: ['--window-size=320,800']
-       },
-       specs: [/*'e2e/onload/onloadCommonSpec.js', 'e2e/onload/onloadMobileTabletSpec.js', 'e2e/footer/footerMobileTabletSpec.js', 'e2e/scrolling/scrollingCommonSpec.js',*/ 'e2e/scrolling/scrollingMobileTabletSpec.js']
-    }/*,
-    { 
-       'browserName': 'chrome',
-       'chromeOptions' : {
-        args: ['--window-size=680,800']
        },
        specs: ['e2e/onload/onloadCommonSpec.js', 'e2e/onload/onloadMobileTabletSpec.js', 'e2e/footer/footerMobileTabletSpec.js', 'e2e/scrolling/scrollingCommonSpec.js', 'e2e/scrolling/scrollingMobileTabletSpec.js']
     },
     { 
        'browserName': 'chrome',
        'chromeOptions' : {
+        args: ['--window-size=680,800']
+       },
+       specs: [/*'e2e/onload/onloadCommonSpec.js', 'e2e/onload/onloadMobileTabletSpec.js', 'e2e/footer/footerMobileTabletSpec.js', 'e2e/scrolling/scrollingCommonSpec.js', 'e2e/scrolling/scrollingMobileTabletSpec.js']
+    },*/
+    { 
+       'browserName': 'chrome',
+       'chromeOptions' : {
         args: ['--window-size=1280,800']
        },
-       specs: ['e2e/onload/onloadCommonSpec.js', 'e2e/onload/onloadDesktopSpec.js', 'e2e/footer/footerDesktopSpec.js', 'e2e/scrolling/scrollingCommonSpec.js', 'e2e/scrolling/scrollingDesktopSpec.js']
-    }  */
+       specs: [/*'e2e/onload/onloadCommonSpec.js', 'e2e/onload/onloadDesktopSpec.js', */'e2e/footer/footerDesktopSpec.js', 'e2e/scrolling/scrollingCommonSpec.js', 'e2e/scrolling/scrollingDesktopSpec.js']
+    }
   ],
   chromeDriver: '../node_modules/protractor/selenium/chromedriver',
   baseUrl: 'http://localhost:8080/',
