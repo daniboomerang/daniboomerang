542455c2a92d00b522d9b1b9cee6bde33a700c69
daniboomerang <estevez.dani@gmail.com>
2015-12-10 00:55:35 +0100

    Social sharing not working with a directive in the head and inserting meta content. Testing with a directive as attribute that inserts the content in the meta element depending of some attribute 'options'.

--- client/components/common/share/share.js
+++ client/components/common/share/share.js
@@ -23,6 +23,31 @@ share.factory('socialSharingService', function (DANIBOOMERANG_URL, SHARE_BASE_UR
   } 
 });
 
+share.directive('metafb', function(socialSharingService) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      if (attrs.og == "title") { element.attr('content', socialSharingService.getSocialTitle())}
+      if (attrs.og == "type") { element.attr('content', socialSharingService.getSocialType())}
+      if (attrs.og == "image") { element.attr('content', socialSharingService.getSocialMedia())}
+      if (attrs.og == "url") { element.attr('content', socialSharingService.getSocialUrl())}
+      if (attrs.og == "description") { element.attr('content', socialSharingService.getSocialDescription())}
+    }
+  }
+});
+
+share.directive('metatw', function(socialSharingService) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      if (attrs.tw == "card") { element.attr('content', 'summary')}
+      if (attrs.tw == "title") { element.attr('content', socialSharingService.getSocialTitle())}
+      if (attrs.tw == "image") { element.attr('content', socialSharingService.getSocialMedia())}
+      if (attrs.tw == "description") { element.attr('content', socialSharingService.getSocialDescription())}
+    }
+  }
+});
+
 share.directive('socialSharingMetadata', function($compile, SHARE_BASE_URL, socialSharingService) {
   return {
     restrict: 'A',
