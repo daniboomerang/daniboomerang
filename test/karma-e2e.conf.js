4be0549ca6725b88adc50f496cfafaff69ea7067
daniboomerang <estevez.dani@gmail.com>
2015-12-17 13:05:33 +0100

    Updating so there are no multicapabilities anymore.

--- test/karma-e2e.conf.js
+++ test/karma-e2e.conf.js
@@ -1,28 +1,5 @@
 exports.config = {
-  /*specs: [], capabilities: { browserName: 'chrome', shardTestFiles: true, maxInstances: 2 },*/
-  multiCapabilities: [  
-    /*{ 
-       'browserName': 'chrome',
-       'chromeOptions' : {
-        args: ['--window-size=320,800']
-       },
-       specs: ['e2e/introSpec.js', 'e2e/parallaxSpec.js']
-    },
-    { 
-       'browserName': 'chrome',
-       'chromeOptions' : {
-        args: ['--window-size=680,800']
-       },
-       specs: ['e2e/introSpec.js', 'e2e/parallaxSpec.js']
-    },*/
-    { 
-       'browserName': 'chrome',
-       'chromeOptions' : {
-        args: ['--window-size=1280,800']
-       },
-       specs: ['e2e/introSpec.js', 'e2e/parallaxSpec.js']
-    }
-  ],
+  specs: ['e2e/introSpec.js', 'e2e/parallaxSpec.js'], capabilities: { browserName: 'chrome', shardTestFiles: true, maxInstances: 2 },
   chromeDriver: '../node_modules/protractor/selenium/chromedriver',
   baseUrl: 'http://localhost:8080/',
   framework: 'jasmine2'
